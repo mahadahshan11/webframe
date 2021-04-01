a4f7c4feef3eabba0a554aa07ac99e9c8e21f95a
mahadahshan11 <maha.snape@gmail.com>
2021-04-01 20:50:08 +0300

    changing the size of the pictures

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ alert('welcom to my page');
 document.write('make yourself at home');
 var username=prompt('Please Enter Your favorite social media','facebook linkedin');
 if(username=='facebook'){
- document.write('<img style="width:100px ;" src="https://www.facebook.com/images/fb_icon_325x325.png">')
+ document.write('<img style="width:500px ;" src="https://www.facebook.com/images/fb_icon_325x325.png">')
  document.write(`<h1>${username}</h1>`)
 } 
 if(username=='linkedin'){
-    document.write('<img style="width:100px ;" src="https://temitopeadelekan.files.wordpress.com/2015/05/linkedin_logo_initials.png">')
+    document.write('<img style="width:500px ;" src="https://temitopeadelekan.files.wordpress.com/2015/05/linkedin_logo_initials.png">')
     document.write(`<h1>${username}</h1>`)
 }
